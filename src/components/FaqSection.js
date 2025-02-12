e67b2b6ef6580473b8ac5092a5248ab9bb354a30
Temur <temur.eshkuwatov@gmail.com>
2025-02-12 21:35:51 +0500

    Faq Section completed

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+    return (
+      <div className="faq">
+        <h2>
+          Any Questions <span>FAQ</span>
+        </h2>
+        <div className="question">
+          <h4>Can I learn programming from 0 on your platform?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. A,
+              dolores..
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>How much do I have to pay per month?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. A,
+              dolores..
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Can I pay for everything at once?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. A,
+              dolores..
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>If I buy the course, can I take it with my brother?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. A,
+              dolores..
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+}
+ 
+export default FaqSection;
