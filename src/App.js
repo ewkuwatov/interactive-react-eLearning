50ec28b5b16a6db8f53cf809c3f054d07bf689c8
Temur <temur.eshkuwatov@gmail.com>
2025-02-12 21:13:49 +0500

    AboutSection html completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <h1>eLearning</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import homeIntro from '../img/home_intro.jpg'
+
+const AboutSection = () => {
+    return (
+      <div>
+        <div className="description">
+          <div className="title">
+            <div className="hide">
+              <h2>Be educated so that</h2>
+            </div>
+            <div className="hide">
+              <h2>you can change</h2>
+            </div>
+            <div className="hide">
+              <h2>the world.</h2>
+            </div>
+          </div>
+          <p>
+            An educated mind can teach many. An educated mind is better than
+            empty one.
+          </p>
+          <button>About Us</button>
+        </div>
+        <div className="image">
+          <img src={homeIntro} alt="childs reading a book" />
+        </div>
+      </div>
+    )
+}
+ 
+export default AboutSection;
